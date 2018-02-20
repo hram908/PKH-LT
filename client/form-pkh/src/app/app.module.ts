8d061b013e3391c1ce84f09b2b90808e5e987b79
Thomas <askat@hab-verschlafen.de>
2018-02-20 16:39:28 +0100

    insurance component added

--- client/form-pkh/src/app/app.module.ts
+++ client/form-pkh/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import {AppComponent} from './app.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
 import { FormData } from './formData';
+import { InsuranceDataComponent } from './insurance-data/insurance-data.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonalDataComponent,
+    InsuranceDataComponent,
   ],
   imports: [
     BrowserModule
---
+++ client/form-pkh/src/app/insurance-data/insurance-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insurance-data',
+  templateUrl: './insurance-data.component.html',
+  styleUrls: ['./insurance-data.component.css']
+})
+export class InsuranceDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
