e6c741dd32e3e1462c3e9fca859c5475292597f3
Isabella Tran <tran.isabella93@googlemail.com>
2018-02-21 11:15:12 +0100

    it: ADD generated component for the chatbot

--- client/form-pkh/src/app/app.module.ts
+++ client/form-pkh/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {AppComponent} from './app.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
 import { FormData } from './formData';
 import { InsuranceDataComponent } from './insurance-data/insurance-data.component';
+import { FormAComponent } from './chatbot/form-a/form-a.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 
 @NgModule({
@@ -11,6 +13,8 @@ import { InsuranceDataComponent } from './insurance-data/insurance-data.componen
     AppComponent,
     PersonalDataComponent,
     InsuranceDataComponent,
+    FormAComponent,
+    ChatbotComponent,
   ],
   imports: [
     BrowserModule
