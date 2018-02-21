66be6e000828356ec32fd623c276e801c7a8ca0b
Thomas <askat@hab-verschlafen.de>
2018-02-21 11:13:04 +0100

    added bootstrap dependencies to package.json moved insurance-data and personal-data in sub folder formular added greeting-window to sub folder formular

--- client/form-pkh/src/app/app.module.ts
+++ client/form-pkh/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AppComponent} from './app.component';
-import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { PersonalDataComponent } from './formular/personal-data/personal-data.component';
 import { FormData } from './formData';
-import { InsuranceDataComponent } from './insurance-data/insurance-data.component';
+import { InsuranceDataComponent } from './formular/insurance-data/insurance-data.component';
+import { GreetingWindowComponent } from './formular/greeting-window/greeting-window.component';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { InsuranceDataComponent } from './insurance-data/insurance-data.componen
     AppComponent,
     PersonalDataComponent,
     InsuranceDataComponent,
+    GreetingWindowComponent,
   ],
   imports: [
     BrowserModule
