7f87cad32d565cdfbd2d15a224a4e6c3b7f86da6
Isabella Tran <tran.isabella93@googlemail.com>
2018-02-23 13:17:16 +0100

    it: ADD pkh form material

--- client/form-pkh/src/app/formulardaten/pkh-form-material.ts
+++ client/form-pkh/src/app/formulardaten/pkh-form-material.ts
@@ -1,3 +1,26 @@
-export class PkhFormMaterial{
+import {FormAMaterial} from './form-a/form-a-material';
+import {FormBMaterial} from './form-b/form-b-material';
+import {FormCMaterial} from './form-c/form-c-material';
+import {FormDMaterial} from './form-d/form-d-material';
+import {FormEMaterial} from './form-e/form-e-material';
+import {FormFMaterial} from './form-f/form-f-material';
+import {FormGMaterial} from './form-g/form-g-material';
+import {FormHMaterial} from './form-h/form-h-material';
+import {FormIMaterial} from './form-i/form-i-material';
+import {FormJMaterial} from './form-j/form-j-material';
 
+/**
+ * @author Isabella
+ **/
+export class PkhFormMaterial{
+abschnittA: FormAMaterial;
+  abschnittB: FormBMaterial;
+  abschnittC: FormCMaterial;
+  abschnittD: FormDMaterial;
+  abschnittE: FormEMaterial;
+  abschnittF: FormFMaterial;
+  abschnittG: FormGMaterial;
+  abschnittH: FormHMaterial;
+  abschnittI: FormIMaterial;
+  abschnittJ: FormJMaterial;
 }
