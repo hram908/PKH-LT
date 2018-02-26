19dba33b0fbd09d9f8f5dacbcca4a7b336f5185d
5synnatz <5synnatz@informatik.uni-hamburg.de>
2018-02-26 14:04:13 +0100

    Started Formula c

--- client/form-pkh/src/app/formular/form-c-unterhaltsanspruch/form-c-unterhaltsanspruch.component.ts
+++ client/form-pkh/src/app/formular/form-c-unterhaltsanspruch/form-c-unterhaltsanspruch.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Untertitel} from '../../common/untertitel';
 import {IFormComponentBase} from '../../common/i-form-component-base';
+import {FormCMaterial} from "../../formulardaten/form-c/form-c-material";
+
 
 @Component({
   selector: 'app-form-c-unterhaltsanspruch',
@@ -8,10 +10,13 @@ import {IFormComponentBase} from '../../common/i-form-component-base';
   styleUrls: ['./form-c-unterhaltsanspruch.component.css']
 })
 export class FormCUnterhaltsanspruchComponent implements IFormComponentBase{
-  constructor() {
+  constructor(public formC: FormCMaterial) {
   }
 
   getUntertitel(): string {
     return Untertitel.C;
   }
 }
+
+
+
