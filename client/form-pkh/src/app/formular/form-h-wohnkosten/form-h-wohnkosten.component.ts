3a84013710a62abc002f260f796b9b742587098e
Keffler <k-adams@hotmail.de>
2018-02-26 17:19:06 +0100

    add H

--- client/form-pkh/src/app/formular/form-h-wohnkosten/form-h-wohnkosten.component.ts
+++ client/form-pkh/src/app/formular/form-h-wohnkosten/form-h-wohnkosten.component.ts
@@ -1,18 +1,24 @@
 import {Component} from '@angular/core';
 import {Untertitel} from '../../common/untertitel';
 import {IFormComponentBase} from '../../common/i-form-component-base';
+import {FormHMaterial} from '../../formulardaten/form-h/form-h-material';
+import {Fragen} from '../../common/fragen';
 
 @Component({
   selector: 'app-form-h-wohnkosten',
   templateUrl: './form-h-wohnkosten.component.html',
   styleUrls: ['./form-h-wohnkosten.component.css']
 })
-export class FormHWohnkostenComponent implements IFormComponentBase{
-  constructor() {
-  }
+export class FormHWohnkostenComponent implements IFormComponentBase {
+  constructor(public formH: FormHMaterial) {
+}
 
   getUntertitel(): string {
-    return Untertitel.H;
+    return Untertitel.I;
+  }
+
+  getFragen() {
+    return Fragen;
   }
 }
 
