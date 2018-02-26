d74fa775d9af4039fe4b24aa01511b430070c7df
Isabella Tran <tran.isabella93@googlemail.com>
2018-02-26 11:33:53 +0100

    it: ADD missing Abschnitte to Stepspeicher

--- client/form-pkh/src/app/stepSpeicher.ts
+++ client/form-pkh/src/app/stepSpeicher.ts
@@ -1,7 +1,20 @@
 import {Abschnitt} from './abschnitt';
 import {StartfensterComponent} from './formular/startfenster/startfenster.component';
 import {FormAPersonendatenComponent} from './formular/form-a-personendaten/form-a-personendaten.component';
+import {FormBVersicherungenComponent} from './formular/form-b-versicherungen/form-b-versicherungen.component';
+import {FormFAbzuegeComponent} from './formular/form-f-abzuege/form-f-abzuege.component';
+import {FormCUnterhaltsanspruchComponent} from './formular/form-c-unterhaltsanspruch/form-c-unterhaltsanspruch.component';
+import {FormJBesondereBelastungenComponent} from './formular/form-j-besondere-belastungen/form-j-besondere-belastungen.component';
+import {NavButtons} from './navigation/nav-buttons';
+import {FormHWohnkostenComponent} from './formular/form-h-wohnkosten/form-h-wohnkosten.component';
+import {FormISonstigeVerpflichtungenComponent} from './formular/form-i-sonstige-verpflichtungen/form-i-sonstige-verpflichtungen.component';
+import {FormEBruttoeinnahmenComponent} from './formular/form-e-bruttoeinnahmen/form-e-bruttoeinnahmen.component';
+import {FormDGewaehrterUnterhaltComponent} from './formular/form-d-gewaehrter-unterhalt/form-d-gewaehrter-unterhalt.component';
+import {FormGEigentumComponent} from './formular/form-g-eigentum/form-g-eigentum.component';
 
+/**
+ * @author Thomas, Isi
+ */
 export class StepSpeicher {
   aktuellerAbschnitt: Abschnitt;
   abschnitte: Abschnitt[] = [];
@@ -9,16 +22,16 @@ export class StepSpeicher {
   constructor() {
     this.abschnitte.push(new Abschnitt('0', 'Beginn', StartfensterComponent));
     this.abschnitte.push(new Abschnitt('A', 'Personendaten', FormAPersonendatenComponent));
-    /*this.abschnitte.push(new Abschnitt('B', 'Beginn2'));
-    this.abschnitte.push(new Abschnitt('C', 'Beginn3'));
-    this.abschnitte.push(new Abschnitt('D', 'Beginn4'));
-    this.abschnitte.push(new Abschnitt('E', 'Beginn5'));
-    this.abschnitte.push(new Abschnitt('F', 'Beginn6'));
-    this.abschnitte.push(new Abschnitt('G', 'Beginn7'));
-    this.abschnitte.push(new Abschnitt('H', 'Beginn8'));
-    this.abschnitte.push(new Abschnitt('I', 'Beginn9'));
-    this.abschnitte.push(new Abschnitt('J', 'Beginn10'));
-    this.abschnitte.push(new Abschnitt('K', 'Beginn11'));*/
+    this.abschnitte.push(new Abschnitt('B', 'Versicherungen', FormBVersicherungenComponent));
+    this.abschnitte.push(new Abschnitt('C', 'Unterhaltsanspruch', FormCUnterhaltsanspruchComponent));
+    this.abschnitte.push(new Abschnitt('D', 'GewaehrterUnterhalt', FormDGewaehrterUnterhaltComponent));
+    this.abschnitte.push(new Abschnitt('E', 'Bruttoeinnahmen', FormEBruttoeinnahmenComponent));
+    this.abschnitte.push(new Abschnitt('F', 'Abzuege', FormFAbzuegeComponent));
+    this.abschnitte.push(new Abschnitt('G', 'Eigentum', FormGEigentumComponent));
+    this.abschnitte.push(new Abschnitt('H', 'Wohnkosten', FormHWohnkostenComponent));
+    this.abschnitte.push(new Abschnitt('I', 'SonstigeVerpflichtungen', FormISonstigeVerpflichtungenComponent));
+    this.abschnitte.push(new Abschnitt('J', 'BesondereBelastungen', FormJBesondereBelastungenComponent));
+  //  this.abschnitte.push(new Abschnitt('K', 'Beginn11'));
 
     this.aktuellerAbschnitt = this.abschnitte[0];
   }
