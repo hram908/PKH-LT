6f60260835654d1273675a1dd8947ec94f0a1486
Isabella Tran <tran.isabella93@googlemail.com>
2018-02-27 10:22:36 +0100

    it: WIP added a service and controller for the pkh form

---
+++ server/pkh-backend/src/main/java/pkh/form/FormularService.java
@@ -0,0 +1,13 @@
+package pkh.form;
+
+import org.springframework.stereotype.Service;
+import pkh.form.materials.PkhFormular;
+
+@Service
+public class FormularService {
+    // TODO
+    public PkhFormular create() {
+        return null;
+    }
+
+}
--- server/pkh-backend/src/main/java/pkh/form/materials/PkhFormular.java
+++ server/pkh-backend/src/main/java/pkh/form/materials/PkhFormular.java
@@ -1,7 +1,90 @@
 package pkh.form.materials;
 
-import javax.persistence.Entity;
+import pkh.form.materials.abschnitte.a.FormA;
+import pkh.form.materials.abschnitte.b.FormB;
+import pkh.form.materials.abschnitte.c.FormC;
+import pkh.form.materials.abschnitte.d.FormD;
+import pkh.form.materials.abschnitte.e.FormE;
+import pkh.form.materials.abschnitte.f.FormF;
+import pkh.form.materials.abschnitte.g.FormG;
+import pkh.form.materials.abschnitte.h.FormH;
+import pkh.form.materials.abschnitte.i.FormI;
+import pkh.form.materials.abschnitte.j.FormJ;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author Isabella
+ */
 public class PkhFormular {
+    private long id;
+    private FormA formA;
+    private FormB formB;
+    private FormC formC;
+    private FormD formD;
+    private FormE formE;
+    private FormF formF;
+    private FormG formG;
+    private FormH formH;
+    private FormI formI;
+    private FormJ formJ;
+
+    public PkhFormular(long id, FormA formA, FormB formB, FormC formC, FormD formD, FormE formE, FormF formF, FormG formG, FormH formH, FormI formI, FormJ formJ) {
+        this.id = id;
+        this.formA = formA;
+        this.formB = formB;
+        this.formC = formC;
+        this.formD = formD;
+        this.formE = formE;
+        this.formF = formF;
+        this.formG = formG;
+        this.formH = formH;
+        this.formI = formI;
+        this.formJ = formJ;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public FormA getFormA() {
+        return formA;
+    }
+
+    public FormB getFormB() {
+        return formB;
+    }
+
+    public FormC getFormC() {
+        return formC;
+    }
+
+    public FormD getFormD() {
+        return formD;
+    }
+
+    public FormE getFormE() {
+        return formE;
+    }
+
+    public FormF getFormF() {
+        return formF;
+    }
+
+    public FormG getFormG() {
+        return formG;
+    }
+
+    public FormH getFormH() {
+        return formH;
+    }
+
+    public FormI getFormI() {
+        return formI;
+    }
+
+    public FormJ getFormJ() {
+        return formJ;
+    }
 }
