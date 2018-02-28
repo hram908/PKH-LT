b164c52514d7c712d469649a7a8e9d8e5195cb85
Thomas <askat@hab-verschlafen.de>
2018-02-28 13:30:18 +0100

    corrected ngFor in D

--- client/form-pkh/src/app/formular/form-d-gewaehrter-unterhalt/form-d-gewaehrter-unterhalt.component.ts
+++ client/form-pkh/src/app/formular/form-d-gewaehrter-unterhalt/form-d-gewaehrter-unterhalt.component.ts
@@ -1,19 +1,16 @@
 import {Component} from '@angular/core';
 import {Untertitel} from '../../common/untertitel';
 import {IFormComponentBase} from '../../common/i-form-component-base';
-import {FormDMaterial} from "../../formulardaten/form-d/form-d-material";
-import {Fragen} from "../../common/fragen";
-import {NaturalunterhaltRow} from "../../formulardaten/form-d/naturalunterhalt-row";
-
+import {FormDMaterial} from '../../formulardaten/form-d/form-d-material';
+import {Fragen} from '../../common/fragen';
 
 @Component({
   selector: 'app-form-d-gewaehrter-unterhalt',
   templateUrl: './form-d-gewaehrter-unterhalt.component.html',
   styleUrls: ['./form-d-gewaehrter-unterhalt.component.css']
 })
-export class FormDGewaehrterUnterhaltComponent implements IFormComponentBase{
-  constructor(public formD: FormDMaterial,
-              public formDNat: NaturalunterhaltRow) {
+export class FormDGewaehrterUnterhaltComponent implements IFormComponentBase {
+  constructor(public formD: FormDMaterial) {
   }
 
   getUntertitel(): string {
