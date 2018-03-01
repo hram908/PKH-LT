c27e5d1f29a6dbf7761e26d1397bddad2921d2fd
Isabella Tran <tran.isabella93@googlemail.com>
2018-03-01 03:40:38 +0100

    it: WIP add dummy buttons in form b component of the chatbot

--- client/form-pkh/src/app/chatbot/chatbot-form-b/chatbot-form-b.component.ts
+++ client/form-pkh/src/app/chatbot/chatbot-form-b/chatbot-form-b.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chatbot-form-b',
   templateUrl: './chatbot-form-b.component.html',
   styleUrls: ['./chatbot-form-b.component.css']
 })
-export class ChatbotFormBComponent implements OnInit {
+export class ChatbotFormBComponent {
+  private readonly DefaultText: string = 'Kann ich Ihnen bei Abschnitt B helfen?';
 
-  constructor() { }
+  public chatbotText: string;
+  public lastUserInput: string;
 
-  ngOnInit() {
+  constructor() {
+    this.chatbotText = this.DefaultText;
+    this.lastUserInput = '';
   }
 
+  public askWatson(userInput: string){
+
+  }
 }
--- client/form-pkh/src/app/chatbot/chatbot-service.ts
+++ client/form-pkh/src/app/chatbot/chatbot-service.ts
@@ -1,6 +1,15 @@
 import {Abschnitt} from '../abschnitt';
 import {Injectable} from '@angular/core';
 import {FormAComponent} from './form-a/form-a.component';
+import {ChatbotFormBComponent} from './chatbot-form-b/chatbot-form-b.component';
+import {ChatbotFormCComponent} from './chatbot-form-c/chatbot-form-c.component';
+import {ChatbotFormDComponent} from './chatbot-form-d/chatbot-form-d.component';
+import {ChatbotFormEComponent} from './chatbot-form-e/chatbot-form-e.component';
+import {ChatbotFormFComponent} from './chatbot-form-f/chatbot-form-f.component';
+import {ChatbotFormGComponent} from './chatbot-form-g/chatbot-form-g.component';
+import {ChatbotFormHComponent} from './chatbot-form-h/chatbot-form-h.component';
+import {ChatbotFormIComponent} from './chatbot-form-i/chatbot-form-i.component';
+import {ChatbotFormJComponent} from './chatbot-form-j/chatbot-form-j.component';
 
 @Injectable()
 export class ChatbotService {
@@ -13,5 +22,15 @@ export class ChatbotService {
 
   private initializeAbschnitte() {
     this.chatbotAbschnitte.push(new Abschnitt('A', 'Beginn', FormAComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('B', 'Versicherungen', ChatbotFormBComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('C', 'Unterhaltsanspruch', ChatbotFormCComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('D', 'GewaehrterUnterhalt', ChatbotFormDComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('E', 'Bruttoeinnahmen', ChatbotFormEComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('F', 'Abzuege', ChatbotFormFComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('G', 'Eigentum', ChatbotFormGComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('H', 'Wohnkosten', ChatbotFormHComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('I', 'SonstigeVerpflichtungen', ChatbotFormIComponent));
+    this.chatbotAbschnitte.push(new Abschnitt('J', 'BesondereBelastungen', ChatbotFormJComponent));
+
   }
 }
