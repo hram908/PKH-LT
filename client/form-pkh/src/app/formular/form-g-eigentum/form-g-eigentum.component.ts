9a07ea6b94fab2ab86e2f91e6c2d07c92213288b
Thomas <askat@hab-verschlafen.de>
2018-03-01 10:53:00 +0100

    added multiple entries to G

--- client/form-pkh/src/app/formular/form-g-eigentum/form-g-eigentum.component.ts
+++ client/form-pkh/src/app/formular/form-g-eigentum/form-g-eigentum.component.ts
@@ -3,6 +3,7 @@ import {Untertitel} from '../../common/untertitel';
 import {IFormComponentBase} from '../../common/i-form-component-base';
 import {FormGMaterial} from '../../formulardaten/form-g/form-g-material';
 import {Fragen} from '../../common/fragen';
+import {EintragG} from '../../formulardaten/form-g/eintrag-g';
 
 @Component({
   selector: 'app-form-g-eigentum',
@@ -19,6 +20,78 @@ export class FormGEigentumComponent implements IFormComponentBase {
 
   getUntertitel(): string {
     return Untertitel.G;
+  }
+
+  fuegeHinzuKonto() {
+    if (this.formG.konto.length <= 4) {
+      this.formG.konto.push(new EintragG());
+    }
+  }
+
+  entferneKonto() {
+    if (this.formG.konto.length > 1) {
+      this.formG.konto.pop();
+    }
+  }
+
+  fuegeHinzuGrundeigentum() {
+    if (this.formG.grundeigentum.length <= 4) {
+      this.formG.grundeigentum.push(new EintragG());
+    }
+  }
+
+  entferneGrundeigentum() {
+    if (this.formG.grundeigentum.length > 1) {
+      this.formG.grundeigentum.pop();
+    }
+  }
+
+  fuegeHinzuKfz() {
+    if (this.formG.kraftfahrzeuge.length <= 4) {
+      this.formG.kraftfahrzeuge.push(new EintragG());
+    }
+  }
+
+  entferneKfz() {
+    if (this.formG.kraftfahrzeuge.length > 1) {
+      this.formG.kraftfahrzeuge.pop();
+    }
+  }
+
+  fuegeHinzuWertgegenstaende() {
+    if (this.formG.wertgegenstaende.length <= 4) {
+      this.formG.wertgegenstaende.push(new EintragG());
+    }
+  }
+
+  entferneWertgegenstaende() {
+    if (this.formG.wertgegenstaende.length > 1) {
+      this.formG.wertgegenstaende.pop();
+    }
+  }
+
+  fuegeHinzuVersicherung() {
+    if (this.formG.versicherung.length <= 4) {
+      this.formG.versicherung.push(new EintragG());
+    }
+  }
+
+  entferneVersicherung() {
+    if (this.formG.versicherung.length > 1) {
+      this.formG.versicherung.pop();
+    }
+  }
+
+  fuegeHinzuSonstiges() {
+    if (this.formG.sonstige.length <= 4) {
+      this.formG.sonstige.push(new EintragG());
+    }
+  }
+
+  entferneSonstiges() {
+    if (this.formG.sonstige.length > 1) {
+      this.formG.sonstige.pop();
+    }
   }
 }
 
