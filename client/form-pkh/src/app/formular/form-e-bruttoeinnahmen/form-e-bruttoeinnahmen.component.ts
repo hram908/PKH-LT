9c7586f15d036ce55c3c97287e6ec6c103e6e58e
Thomas <askat@hab-verschlafen.de>
2018-03-01 11:51:24 +0100

    tried fixing problems in E, closed ng-container

--- client/form-pkh/src/app/formular/form-e-bruttoeinnahmen/form-e-bruttoeinnahmen.component.ts
+++ client/form-pkh/src/app/formular/form-e-bruttoeinnahmen/form-e-bruttoeinnahmen.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
-import {Untertitel} from '../../common/untertitel';
 import {IFormComponentBase} from '../../common/i-form-component-base';
-import {Fragen} from '../../common/fragen';
 import {FormEMaterial} from '../../formulardaten/form-e/form-e-material';
+import {Fragen} from '../../common/fragen';
+import {Untertitel} from '../../common/untertitel';
 
 @Component({
   selector: 'app-form-e-bruttoeinnahmen',
@@ -13,11 +13,11 @@ export class FormEBruttoeinnahmenComponent implements IFormComponentBase {
   constructor(public formE: FormEMaterial) {
   }
 
-  getUntertitel(): string {
-    return Untertitel.E;
-  }
-
   getFragen() {
     return Fragen;
+  }
+
+  getUntertitel(): string {
+    return Untertitel.E;
   }
 }
