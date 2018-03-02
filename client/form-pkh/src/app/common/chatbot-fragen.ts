6f0eb0c0e92c22dc3eca348cf97e196c1451ef51
Isabella Tran <tran.isabella93@googlemail.com>
2018-03-02 10:40:23 +0100

    it: ADD chatbot question added

--- client/form-pkh/src/app/common/chatbot-fragen.ts
+++ client/form-pkh/src/app/common/chatbot-fragen.ts
@@ -1,7 +1,51 @@
-export class ChatbotFragen{
+export class ChatbotFragen {
   public static readonly A = {
-    DEFAULT: 'Wie kann ich Ihnen bezüglich Abschnitt A helfen?',
-    A1: ''
+    A1: 'Was gebe ich an, wenn ich bei X in der Garage Wohne?',
+    A2: 'Was gebe ich an, wenn ich obdachlos bin?'
+  };
+
+  public static readonly B = {
+    B1: 'Woher weiß ich, dass ich eine Rechtsschutzversicherung habe?'
+  };
+
+  public static readonly D = {
+    D1: 'Was ist mir Bar- und/oder Naturalunterhalt gemeint?',
+    D2: 'Wer zählt zu Angehörigen?'
   }
 
+  public static readonly E = {
+    E1: 'Was bedeutet Verpachtung?',
+    E2: 'Was ist Kapitalvermögen?',
+    E3: 'Ich bekomme Rente und Pension, wie trage ich das ein?',
+    E4: 'Was ist mit andere Einnahmen gemeint?'
+  }
+
+  public static readonly F = {
+    F1: 'Was ist mit Solidaritätszuschlag gemeint?',
+    F2: 'Was wäre eine sonstige Versicherung?',
+    F3: 'Was ist mit einfache Entfernung bei KFZ-Nutzung gemeint?',
+    F4: 'Was ist mit Werbungskosten gemeint?'
+  }
+
+  public static readonly G = {
+    G1: 'Ab wann ist Schmuck wertvoll?',
+    G2: 'Ab wann ist ein elektonisches Gerät hochwertig?',
+    G3: 'Ab welchem Wert muss ich Bargeld angeben?'
+  }
+
+  public static readonly H = {
+    H1: 'Was ist, wenn ich obdachlos bin?',
+    H2: 'Was sind Nutzungsverhältnisse?',
+    H3: 'Was ist Erbbauberechtigung?'
+  }
+
+  public static readonly I = {
+    I1: 'Was ist mit sonstige Zahlungsverpflichtungen gemeint?'
+  }
+
+  public static readonly J = {
+    J1: 'Ab wann ist jemand behindert?',
+    J2: 'Was steht in § 21 SGB II ?',
+    J3: 'Was steht in § 30 SGB XII?'
+  }
 }
