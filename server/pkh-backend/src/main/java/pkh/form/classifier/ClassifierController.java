3622c1717e30d995f4a7aab4a4efa19d0c4bcd3c
5synnatz <5synnatz@informatik.uni-hamburg.de>
2018-03-07 15:15:25 +0100

    classifier tool curl request tested funktioning

--- server/pkh-backend/src/main/java/pkh/form/classifier/ClassifierController.java
+++ server/pkh-backend/src/main/java/pkh/form/classifier/ClassifierController.java
@@ -1,10 +1,11 @@
-/*package pkh.form.classifier;
+package pkh.form.classifier;
 
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
+import pkh.form.classifier.ClassifierService;
 
 import java.util.List;
 
@@ -27,4 +28,3 @@ public class ClassifierController {
         return response;
     }
 }
-*/
--- server/pkh-backend/src/main/java/pkh/form/classifier/ClassifierService.java
+++ server/pkh-backend/src/main/java/pkh/form/classifier/ClassifierService.java
@@ -1,4 +1,4 @@
-/*package pkh.form.classifier;
+package pkh.form.classifier;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.*;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
 import org.springframework.stereotype.Service;
@@ -31,4 +31,3 @@ public class ClassifierService {
     }
 
 }
-*/
